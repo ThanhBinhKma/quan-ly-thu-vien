71ba4868b6b907a1727c8fd1e4130734e547e022
ThanhBinhKma <Thanhbinhkma27@gmail.com>
2020-10-20 21:29:36 +0700

    ea

--- resources/views/home/aside.blade.php
+++ resources/views/home/aside.blade.php
@@ -24,7 +24,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
+                    <a href="{{route('user.index')}}" class="nav-link">
                         <i class="nav-icon far fa-envelope"></i>
                         <p>
                             User
@@ -33,7 +33,7 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="pages/mailbox/mailbox.html" class="nav-link">
+                            <a href="{{route('user.index')}}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>List User</p>
                             </a>
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Auth::routes(['verify' => true]);
 
 Route::group(['middleware'=>['verified']],function(){
     Route::get('/home', 'HomeController@index')->name('home');
+
+    Route::get('user','UserController@index')->name('user.index');
 });
